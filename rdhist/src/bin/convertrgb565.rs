46aae430682f22bbc4142acaf04633f35e904a24
Fabien Marteau <mail@fabienm.eu>
2021-11-07 21:05:16 +0100

    adding some convert function not functionnal

--- rdhist/src/bin/convertrgb565.rs
+++ rdhist/src/bin/convertrgb565.rs
@@ -1,5 +1,17 @@
-use std::fs;
+extern crate image;
+
+use std::io;
+use std::error::Error;
+use image::io::Reader as ImageReader;
 
 fn main() {
     println!("Converting image to rgb565 format");
+    test_load_img().unwrap();
 }
+
+fn test_load_img() {
+    let img = ImageReader::open("colere_sven.png")?.decode()?;
+    img.save("colere_sven.jpg")?;
+}
+
+
--- rdhist/src/main.rs
+++ rdhist/src/main.rs
@@ -1,5 +1,5 @@
 extern crate yaml_rust;
-use yaml_rust::{YamlLoader, YamlEmitter};
+use yaml_rust::{YamlLoader};
 use std::fs;
 
 fn main() {
