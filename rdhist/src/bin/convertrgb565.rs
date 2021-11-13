88566184d98a60b05a031e9b8f3635e517ba214b
Fabien Marteau <mail@fabienm.eu>
2021-11-13 16:56:19 +0100

    base for binary img writing

--- rdhist/src/bin/convertrgb565.rs
+++ rdhist/src/bin/convertrgb565.rs
@@ -1,4 +1,6 @@
 extern crate image;
+use std::fs::File;
+use std::io::Write;
 
 use image::GenericImageView;
 
@@ -11,9 +13,20 @@ fn main() {
 fn test_load_img() {
     println!("Open colere_sven.png");
     let img = image::open("colere_sven.png").unwrap();
-    println!("dimensions {:?}", img.dimensions());
+    let (wimg, himg) = img.dimensions();
     println!("{:?}", img.color());
     img.save("colere_sven.jpg").unwrap();
+
+    let imgrgb = img.into_rgb8();
+
+    for pix in imgrgb.pixels() {
+        println!("Pixel {:?} -> {:x}", pix, pix[0]);
+    }
+
+    println!("width {:?}, height {:?}", wimg, himg);
+
+    let mut outfile = File::create("output.bin").unwrap();
+    outfile.write(&[0x40u8]).unwrap();
 }
 
 
