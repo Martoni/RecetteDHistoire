df7a973866c396db85e25e7a1a41cdea53fbf0ed
Fabien Marteau <mail@fabienm.eu>
2022-09-10 09:11:54 +0200

    décodage de la table des matières

--- rdhist/src/rdsound/cdaudio.rs
+++ rdhist/src/rdsound/cdaudio.rs
@@ -2,20 +2,94 @@
 /// Les programmes cdparanoia et cd-discid sont requis.
 use std::error::Error;
 use std::process::Command;
+use std::time::Duration;
 use std::str;
+
+
+const CMD_CDDISCID: &str = "cd-discid";
+const CMD_CDPARANOIA: &str = "cdparanoia";
 
 /// Lit le numéro unique du CD se trouvant dans le lecteur
 pub fn get_discid() -> Result<String, Box<dyn Error>> {
-    let _cmdret = Command::new("cd-discid")
-                            .output()
-                            .expect("La commande cd-discid a échouée");
+    let _cmdret = Command::new(CMD_CDDISCID)
+                        .output()
+                        .expect(&format!("La commande {CMD_CDDISCID} a échoué"));
 
     Ok(str::replace(str::from_utf8(&_cmdret.stdout)?,"\n",""))
 }
 
+pub fn get_toc() -> Result<String, Box<dyn Error>> {
+    let _cmdret = Command::new(CMD_CDPARANOIA)
+                        .args(["-Q"])
+                        .output()
+                        .expect(&format!("La commande {CMD_CDPARANOIA} a échoué"));
+
+    // La table des matières est affichée sur le canal d'erreur
+    // dans cdparanoia !
+    let sret = _cmdret.stderr;
+
+    Ok(str::from_utf8(&sret)?.to_string())
+}
+
+pub fn parse_duration(raw_duration: &str) -> Result<Duration, Box<dyn Error>> {
+    let min  = raw_duration[1..3].parse::<u64>()?;
+    let sec  = raw_duration[4..6].parse::<u64>()?;
+    let dsec = raw_duration[7..9].parse::<u64>()?;
+    Ok(Duration::from_secs(min*60)
+        + Duration::from_secs(sec)
+        + Duration::from_millis(dsec*10))
+}
+
+pub fn parse_toc(ntrack: u8, raw_toc: &str) -> Result<Vec<Duration>, Box<dyn Error>> {
+    let mut iter_toc = raw_toc.lines();
+    let mut line = " ";
+    while line.chars().nth(1) != Some('=') {
+        line = iter_toc.next().unwrap();
+    }
+    let mut tracks = Vec::<Duration>::new();
+    for _track in 1..=ntrack {
+        line = iter_toc.next().unwrap();
+        let vec_track: Vec<&str> = line.split_whitespace().collect();
+        tracks.push(parse_duration(vec_track[2])?);
+    }
+    Ok(tracks)
+}
+
+///---------------------
+/// Table Of Content
+///---------------------
+
+pub struct CdToc {
+    pub ntracks: u8,
+    pub tracks: Vec<Duration>,
+}
+
+impl CdToc {
+    pub fn in_drive() -> Result<CdToc, Box<dyn Error>> {
+        // Récupération du discid pour avoir le nombre de pistes
+        let discid = DiscId::in_drive()?;
+        // lecture de la table des matières «brute»
+        let raw_toc = get_toc()?;
+        let ntracks = discid.tracks_number()?;
+        Ok(CdToc {
+            ntracks: ntracks,
+            tracks: parse_toc(ntracks, &raw_toc)?,
+        })
+    }
+}
+
+///----------------------
+/// DiscID
+
 /// Structure de «stockage» du discid
 pub struct DiscId {
-    pub _cddiscid: String,
+    _cddiscid: String,
+}
+
+impl PartialEq for DiscId {
+    fn eq(&self, other: &Self) -> bool {
+        self._cddiscid == other._cddiscid
+    }
 }
 
 impl DiscId {
@@ -39,7 +113,7 @@ impl DiscId {
 
     pub fn vectorized(&self) -> Vec<&str> {
         let ret: Vec<&str> = self._cddiscid.split(' ').collect();
-        ret 
+        ret
     }
 
     pub fn hashid(&self) -> Result<&str, Box<dyn Error>> {
--- rdhist/src/rdsound/test_cdaudio.rs
+++ rdhist/src/rdsound/test_cdaudio.rs
@@ -1,17 +1,37 @@
 /// Test à lancer avec :
 /// $ RUST_BACKTRACE=1 cargo test -- --nocapture
 ///     ^                               ^
-///     |  pour avoir les println! -----|
+///     |     pour avoir les println! --|
 ///     |- pour avoir la trace en cas d'erreur
+
+#[allow(dead_code)]
+const DISCID_SAMPLE: &str = "30027f05 5 150 2422 15072 29219 44022 641";
+#[allow(dead_code)]
+const TOC_PANTHÈRE: &str = "cdparanoia III release 10.2 (September 11, 2008)
+
+ 
+
+Table of contents (audio tracks only):
+track        length               begin        copy pre ch
+===========================================================
+  1.     2272 [00:30.22]        0 [00:00.00]    no   no  2
+  2.    12650 [02:48.50]     2272 [00:30.22]    no   no  2
+  3.    14147 [03:08.47]    14922 [03:18.72]    no   no  2
+  4.    14803 [03:17.28]    29069 [06:27.44]    no   no  2
+  5.     4069 [00:54.19]    43872 [09:44.72]    no   no  2
+TOTAL   47941 [10:39.16]    (audio only)
+ 
+";
+
 #[cfg(test)]
-mod tests {
+mod test_disc_id {
     use crate::rdsound::cdaudio::*;
+    use super::*;
 
     /// les tests suivant fonctionne avec le cd
     /// de mes premiers j'aime lire titré
-    /// Tiens bon, petite panthère ! 
+    /// Tiens bon, petite panthère !
     /// numéro 234 de Février 2022
-    const DISCID_SAMPLE: &str = "30027f05 5 150 2422 15072 29219 44022 641";
 
     #[test]
     fn test_get_discid() {
@@ -31,7 +51,7 @@ mod tests {
         let discid = DiscId::from_string(String::from(DISCID_SAMPLE))
                         .expect("Impossible de récupérer le discid du cd");
         let hashid = discid.hashid().expect("Impossible d'extraire le hashid");
-        assert_eq!(hashid, "30027f05"); 
+        assert_eq!(hashid, "30027f05");
     }
 
     #[test]
@@ -41,5 +61,39 @@ mod tests {
         let tracksnum = discid.tracks_number()
                         .expect("Impossible de décoder le nombre de pistes");
         assert_eq!(tracksnum, 5);
+    }
+
+    #[test]
+    fn test_equality(){
+        let discid1 = DiscId::from_string(String::from(DISCID_SAMPLE))
+                        .expect("Impossible de récupérer le discid du cd");
+        let discid2 = DiscId::from_string(String::from(DISCID_SAMPLE))
+                        .expect("Impossible de récupérer le discid du cd");
+        assert!(discid1 == discid2);
+    }
+}
+
+#[cfg(test)]
+mod test_table_of_content {
+    use crate::rdsound::cdaudio::*;
+    use super::*;
+
+    #[test]
+    fn test_get_toc() {
+       let stoc = get_toc().expect("get_toc a échoué");
+       assert!(stoc == TOC_PANTHÈRE);
+    }
+
+    #[test]
+    fn test_parse_toc() {
+        let ptoc = parse_toc(5, TOC_PANTHÈRE).expect("Problème de parse");
+        println!("{:?}", ptoc);
+        assert!(true);
+    }
+
+    #[test]
+    fn test_in_drive() {
+        let _toc = CdToc::in_drive();
+        assert!(true);
     }
 }
